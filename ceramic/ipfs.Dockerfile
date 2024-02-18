501f5d71d835c0e7454771bb316e2f36a917e12b
grmkris <kristjan.grm1@gmail.com>
2024-02-18 21:48:37 +0100

    frenzie.fun

---
+++ ceramic/ipfs.Dockerfile
@@ -0,0 +1,6 @@
+FROM ceramicnetwork/go-ipfs-daemon:latest
+
+# https://developers.ceramic.network/docs/protocol/js-ceramic/guides/ceramic-nodes/running-cloud#example-with-docker-containers
+# https://railway.app/template/PhPjgz
+
+
--- scripts/commands.mjs
+++ scripts/commands.mjs
@@ -20,6 +20,8 @@ export const RunCommands = async () => {
       did,
     };
   };
+
+
   const generateLocalConfig = async (adminSeed, adminDid) => {
     const configData = {
       anchor: {},
@@ -67,7 +69,58 @@ export const RunCommands = async () => {
       }
     });
   };
+
+  const generateProductionConfig = async (adminSeed, adminDid) => {
+    const configData = {
+      anchor: {},
+      "http-api": {
+        "cors-allowed-origins": [".*"],
+        "admin-dids": [adminDid.id],
+      },
+      ipfs: {
+        mode: "external",
+        "api-endpoint": "http://localhost:5001",
+      },
+      logger: {
+        "log-level": 2,
+        "log-to-files": false,
+      },
+      metrics: {
+        "metrics-exporter-enabled": false,
+        "metrics-port": 9090,
+      },
+      network: {
+        name: "mainnet",
+      },
+      node: {},
+      "state-store": {
+        mode: "fs",
+        "local-directory": `${process.cwd()}/.ceramic/.ceramic/statestore/`,
+      },
+      indexing: {
+        db: `sqlite://${process.cwd()}/.ceramic/indexing.sqlite`,
+        "allow-queries-before-historical-sync": true,
+        models: [],
+      },
+    };
+    writeFile(
+        `${process.cwd()}/composedb_prod.config.json`,
+        JSON.stringify(configData, null, 2),
+        (err) => {
+          if (err) {
+            console.error(err);
+          }
+        },
+    );
+    writeFile(`${process.cwd()}/admin_seed.txt`, adminSeed, (err) => {
+      if (err) {
+        console.error(err);
+      }
+    });
+  }
+
   const { seed, did } = await generateAdminKeyDid();
   await generateLocalConfig(seed, did);
+  await generateProductionConfig(seed, did);
 };
 RunCommands();
